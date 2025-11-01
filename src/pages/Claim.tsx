c297c6a10f6d6f3e58fa0f038fe66db56da40d64
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-11-02 00:36:48 +0330

    minor

--- src/pages/Claim.tsx
+++ src/pages/Claim.tsx
@@ -286,6 +286,7 @@ export const ClaimPage = () => {
               clearInterval(pollingIntervalRef.current);
               pollingIntervalRef.current = null;
             }
+            pollingIntervalRef.current = null;
             pollingStartTimeRef.current = null;
 
             setState((prevState) => ({
@@ -350,17 +351,6 @@ export const ClaimPage = () => {
             }
           } catch (error) {
             console.error("Error claiming tokens:", error);
-            if (pollingIntervalRef.current) {
-              clearInterval(pollingIntervalRef.current);
-              pollingIntervalRef.current = null;
-            }
-            pollingStartTimeRef.current = null;
-
-            setState((prevState) => ({
-              ...prevState,
-              claimLoading: false,
-              isPollingAttestBurn: false,
-            }));
           }
         }
       } catch {}
